cc3e532530a4d928d2055514cf4ec47f8e3ebae8
hkmztrk <hkm.ozturk@gmail.com>
2018-10-10 15:20:28 +0300

    Update Q5.c

--- week03/Q5.c
+++ week03/Q5.c
@@ -1,9 +1,31 @@
-char ch; int num;
 
-scanf("%c", &ch);
-if (('0'<=ch) && (ch<='9')) {
-	num=ch-'0';
-	printf("Twice input is %d \n", 2*num);
+/*
+Read one character as input.
+
+Check if it is a digit
+   +If so, convert it to an integer and display twice that number
+   +o/w display an error message
+
+input: 9
+output: 18
+
+input: a
+output: error
+
+*/
+#include <stdio.h>
+
+int main(void){
+
+	char ch; int num;
+
+	scanf("%c", &ch);
+	if (('0'<=ch) && (ch<='9')) {
+		num=ch-'0';
+		printf("Twice input is %d \n", 2*num);
+	}
+	else
+		printf("Input is not a digit! \n");
 }
-else
-	printf("Input is not a digit! \n");
+
+
