9c04ad7efc1efccfa66a4812a59a88d52b8fcda6
hkmztrk <hkm.ozturk@gmail.com>
2018-11-07 21:13:57 +0300

    Create MTQ4.c

---
+++ week07/MTQ4.c
@@ -0,0 +1,34 @@
+
+#include <stdio.h>
+
+int main() {
+
+
+	int N, i,j;
+	scanf("%d",&n);
+  
+  int spaces = 3N - 4;
+
+	for(i=1;i<=n;i++){
+		for(j=1;j<i;j++){
+			printf(" ");
+		}
+		printf("*");
+
+		for(j=1;j<=spaces;j++){
+			if(i!=n){
+				printf(" ");
+			}
+			else{
+				printf("*");
+			}
+		}
+
+    spaces -=2; 
+		printf("*\n");
+    
+	}
+
+	return 0;
+
+}
