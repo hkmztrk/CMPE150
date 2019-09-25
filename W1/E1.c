52263ac6af176475e3745d198e990528f808dd97
hkmztrk <hkm.ozturk@gmail.com>
2019-09-25 19:25:11 +0300

    week1 materials

---
+++ W1/E1.c
@@ -0,0 +1,33 @@
+/*
+ * This
+ * is a
+ * comment
+ * block
+ */
+
+#include <stdio.h> //input-output (io) library
+#include <stdlib.h>
+
+
+int main(void) {
+
+
+	printf("Hello world");
+
+	int firstVariable; // I declared an integer variable
+
+	firstVariable = 13; // I assigned a value to the variable
+
+	int secondVar = 7; // I initialized a variable with a value
+
+	int mul=firstVariable+secondVar;
+
+	printf("%d + %d = %d",firstVariable, secondVar , mul);
+
+	/*  %d indicates that
+	 *  it will be replaced with an integer (int)
+	 */
+
+
+	return 0;
+}
---
+++ W1/README.md
@@ -0,0 +1,28 @@
+## Week 1
+
+
+### Materials:
+[CMPE150 Official Sources](https://programming.cmpe.boun.edu.tr/)
+[C Tutorial](http://www.learn-c.org/)
+[Online C Compiler] (https://www.onlinegdb.com/online_c_compiler)
+
+### AI-related videos
+
+[What is Artificial Intelligence (or Machine Learning)?](https://www.youtube.com/watch?v=mJeNghZXtMo)
+[The incredible inventions of intuitive AI | Maurice Conti]()
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
